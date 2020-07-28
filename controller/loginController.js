802c9335fd5ba77c98e6f5dab910920cc7614410
S. M. Jubayer <sadatjubayer@gmail.com>
2020-07-28 20:21:58 +0600

    login controller

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,8 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
+const users = require('../data');
+const { forEach } = require('../data');
+// console.log(users);
+
 router.get('/', (req, res) => {
   res.render('login/index');
+});
+
+router.post('/', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Check empty or not
+  if (!username || !password) {
+    return res.send('All Fields are required!');
+  }
+
+  // Check validity
+
+  users.forEach((user) => {
+    // Check username
+    if (user.username === username) {
+      // Check password
+      if (user.password === password) {
+        // Check user type
+        if (user.isAdmin) return res.redirect('/admin');
+        res.redirect('/employee');
+      } else {
+        res.send('Invalid password');
+      }
+    } else {
+      res.send('user not found');
+    }
+  });
 });
 
 module.exports = router;
---
+++ data.js
@@ -0,0 +1,28 @@
+module.exports = [
+  {
+    id: '101',
+    username: 'sadat',
+    password: '1234',
+    isAdmin: true,
+    userData: {
+      phone: '01754422008',
+      email: 'sadat@test.com',
+      address: 'Dhaka',
+      gender: 'male',
+      designation: 'manager',
+    },
+  },
+  {
+    id: '102',
+    username: 'jubayer',
+    password: '1234',
+    isAdmin: false,
+    userData: {
+      phone: '01754422008',
+      email: 'jubayer@test.com',
+      address: 'Kishoreganj',
+      gender: 'male',
+      designation: 'manager',
+    },
+  },
+];
