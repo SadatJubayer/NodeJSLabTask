d1c4f929eb714b2de4589ed9a388da68f8d005f0
S. M. Jubayer <sadatjubayer@gmail.com>
2020-07-28 19:56:35 +0600

    basic server setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const boydParser = require('body-parser');
+const bodyParser = require('body-parser');
+
+// Routers
+
+// App initialization
+const app = express();
+
+// Config
+const PORT = 5000;
+app.set('view engine', 'ejs');
+
+// Middleware
+app.use(bodyParser());
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running at ', PORT);
+});
